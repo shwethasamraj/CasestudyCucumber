39575a1f8fb135f930c0f1d5fdf9a7fe049629bb
shwethasamraj <52441892+shwethasamraj@users.noreply.github.com>
2019-07-03 15:33:17 +0530

    Update RunCukesTest.java

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/resources/Case4.feature"},glue="skeleton",plugin="json:target/jsonreport.json")
+@CucumberOptions(features={"src/test/resources/Case1.feature","src/test/resources/Case2.feature","src/test/resources/Case3.feature","src/test/resources/Case4.feature"},glue="skeleton",plugin="json:target/jsonreport.json")
 public class RunCukesTest {
 }
